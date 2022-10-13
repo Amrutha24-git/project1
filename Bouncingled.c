ab5e03ba22b8d3b5c6f8377bc413d9d07940c392
Amrutha24-git <115610495+Amrutha24-git@users.noreply.github.com>
2022-10-13 15:26:40 +0530

    Update Bouncingled.c

--- Bouncingled.c
+++ Bouncingled.c
@@ -45,14 +45,14 @@ void run_right(){
 	  k=0x01; 
 	  for(j=0;j<=l;j++){
 	  Bits1_PutVal(k ^ 0x0f);
-	  for(i=0;i<=0x7ffff;i++);
+	  for(i=0;i<=0x7fff;i++);
 	  k=k<<1;
 	}  	 	
 }
 void run_left(){
 	  for(j=0;j<=l;j++){
 	  Bits1_PutVal(k ^ 0x0f);	
-	  for(i=0;i<=0x7ffff;i++);
+	  for(i=0;i<=0x7fff;i++);
 	  k=k>>1; 
 	  }
 	
