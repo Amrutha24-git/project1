96b1edaa59ea422df68e43a52cd745b4a2fcd2fb
Amrutha24-git <115610495+Amrutha24-git@users.noreply.github.com>
2022-10-13 16:00:21 +0530

    Update runningledright.c

--- runningledright.c
+++ runningledright.c
@@ -45,7 +45,7 @@ int main(void)
 /*lint -restore Enable MISRA rule (6.3) checking. */
 {
   /* Write your local variable definition here */
-unsigned char i;
+unsigned int i;
 int j,k;
 
   /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
